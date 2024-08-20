3ad8a44b3abee9037a0f737d3cbdbb3d333c78ab
hamtsu <itseatham@gmail.com>
2024-08-20 22:08:20 +1000

    Added japan flag icon

---
+++ src/components/Icons/JapanFlag.tsx
@@ -0,0 +1,17 @@
+const JapanFlag = () => {
+  return (
+    <svg
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="inline"
+    >
+      <rect x="7" y="16" width="46" height="28" rx="2" fill="#F4F4F4" />
+      <circle cx="30" cy="30" r="8" fill="#F94444" />
+    </svg>
+  );
+};
+
+export default JapanFlag;
