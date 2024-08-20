c6ca3f98b0b044004e58c059b8909608252690aa
hamtsu <itseatham@gmail.com>
2024-08-20 22:09:21 +1000

    Added hiragana background pattern component

---
+++ src/components/HiraganaBackground.tsx
@@ -0,0 +1,22 @@
+import Exclamation from "./Icons/Exclamation";
+import JapanFlag from "./Icons/JapanFlag";
+
+const HiriganaBackground = () => {
+  return (
+    <div
+      className="bg-neutral-850 rounded-md p-3 px-14 w-full overflow-clip text-6xl text-neutral-200 font-bold flex items-center animate-fade-in-late"
+      style={{
+        backgroundImage: "url('./../../hiragana-bg.svg')", // Path to your image
+        backgroundRepeat: "repeat",
+        backgroundSize: "340px 300px",
+      }}
+    >
+      <p>
+        A set of useful tools for learning the <JapanFlag /> <b>Japanese language</b>
+        <Exclamation />
+      </p>
+    </div>
+  );
+};
+
+export default HiriganaBackground;
