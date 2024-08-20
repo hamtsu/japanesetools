5c727c7c3af13095937fa854a3add2a4ff016edf
hamtsu <itseatham@gmail.com>
2024-08-20 22:08:48 +1000

    Added exclamation icon

---
+++ src/components/Icons/Exclamation.tsx
@@ -0,0 +1,70 @@
+const Exclamation = () => {
+  return (
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      className="inline"
+    >
+      <path
+        d="M12 9.20392L24.6112 10L25.3039 36.831L15.8078 37.1055L12 9.20392Z"
+        fill="url(#pattern0_4_16)"
+      />
+      <rect
+        x="17.6014"
+        y="43.6599"
+        width="7.89659"
+        height="7.89659"
+        transform="rotate(-1.65586 17.6014 43.6599)"
+        fill="#D9D9D9"
+      />
+      <path
+        d="M34.81 6L46.5 9.5L42.3142 35.7311L32.9554 34.0991L34.81 6Z"
+        fill="url(#pattern1_4_16)"
+      />
+      <rect
+        x="33.2361"
+        y="40.6459"
+        width="8.09881"
+        height="8.09881"
+        transform="rotate(9.89156 33.2361 40.6459)"
+        fill="#D9D9D9"
+      />
+      <defs>
+        <pattern
+          id="pattern0_4_16"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1"
+        >
+          <use
+            xlinkHref="#image0_4_16"
+            transform="matrix(0.113532 0 0 0.0409836 -0.533602 -0.26257)"
+          />
+        </pattern>
+        <pattern
+          id="pattern1_4_16"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1"
+        >
+          <use
+            xlinkHref="#image0_4_16"
+            transform="matrix(0.114286 0 0 0.0409836 -0.537143 -0.26257)"
+          />
+        </pattern>
+        <image
+          id="image0_4_16"
+          width="20"
+          height="31"
+         xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAfCAYAAADnTu3OAAAAAXNSR0IArs4c6QAAADlJREFUSEvt0qEVAAAIQkHdf0Km0W4E46dDuEdLmnpMMxhrYhgTFoYYGgLcxkA7FQwxNAS4jYF2KgsXdm3WcgFFygAAAABJRU5ErkJggg=="
+        />
+      </defs>
+    </svg>
+  );
+};
+
+export default Exclamation;
