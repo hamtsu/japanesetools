94caeb2abf11322d617a6c83c8c656a742e689a8
Hammy <68724706+hamtsu@users.noreply.github.com>
2024-09-03 16:56:08 +1000

    fix: rename vite base for gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  base: "/japanesetools/",
+  base: "/kunren/",
   plugins: [react()],
 }));
