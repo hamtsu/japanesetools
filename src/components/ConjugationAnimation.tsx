0c8df41642da9f8015158ad0aece8035e9c524ad
Hammy <68724706+hamtsu@users.noreply.github.com>
2024-09-06 14:10:55 +1000

    fix: updated conjugation animation component to work with irregular verbs

--- src/components/ConjugationAnimation.tsx
+++ src/components/ConjugationAnimation.tsx
@@ -2,10 +2,19 @@ import { FC, useEffect, useState } from "react";
 import { FaArrowRight } from "react-icons/fa6";
 
 type ConjugationAnimationProps = {
-  verbs: { base: string; firstEnding: string; secondEnding: string, type: string }[];
+  verbs: {
+    base: string;
+    firstEnding: string;
+    secondEnding: string;
+    type: string;
+  }[];
+  wider?: boolean;
 };
 
-const ConjugationAnimation: FC<ConjugationAnimationProps> = ({ verbs }) => {
+const ConjugationAnimation: FC<ConjugationAnimationProps> = ({
+  verbs,
+  wider,
+}) => {
   const [isTransformed, setIsTransformed] = useState(false);
   const [_, setTransformedTimes] = useState(0);
   const [currentVerb, setCurrentVerb] = useState<{
@@ -28,7 +37,8 @@ const ConjugationAnimation: FC<ConjugationAnimationProps> = ({ verbs }) => {
       setIsTransformed((prev) => !prev);
       setTransformedTimes((prev) => {
         const newTimes = prev + 1;
-        if (newTimes % 4 === 0) { // change verb every 4 times
+        if (newTimes % 4 === 0) {
+          // change verb every 4 times
           changeVerb(newTimes / 4);
         }
         return newTimes;
@@ -39,22 +49,33 @@ const ConjugationAnimation: FC<ConjugationAnimationProps> = ({ verbs }) => {
   }, [verbs]);
 
   return (
-    <div className="flex items-start justify-start">
+    <div className={`flex items-start justify-start ${wider && "w-[300px]"}`}>
       <span className="text-5xl font-bold">
-        {currentVerb.base}
+        {currentVerb.base !== "く" &&
+          (currentVerb.base !== "す" &&
+            
+            currentVerb.base)}
         <span
           className={`inline-block transition-all duration-500 ${
-            isTransformed ? "animate-fade-in text-green-500" : "text-red-500 animate-pulse"
+            isTransformed
+              ? "animate-fade-in text-green-500"
+              : "text-red-500 animate-pulse"
           }`}
         >
-          {isTransformed ? currentVerb.secondEnding : currentVerb.firstEnding}
+          {isTransformed
+            ? currentVerb.secondEnding
+            : (currentVerb.base === "く" && "くる") ||
+              (currentVerb.base === "す" && "する") ||
+              currentVerb.firstEnding}
         </span>
 
         <span className="flex gap-2 font-bold text-lg items-center mt-3">
-             {currentVerb.firstEnding} <FaArrowRight className="opacity-70" /> {currentVerb.secondEnding}
-
-             <span className="opacity-50">{currentVerb.type}</span>
-          </span>
+          {(currentVerb.base === "く" && "くる") ||
+            (currentVerb.base === "す" && "する") ||
+            currentVerb.firstEnding}
+          <FaArrowRight className="opacity-70" /> {currentVerb.secondEnding}
+          <span className="opacity-50">{currentVerb.type}</span>
+        </span>
       </span>
     </div>
   );
